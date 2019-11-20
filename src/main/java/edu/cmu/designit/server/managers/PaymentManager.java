31277ee264c638e103d5b89da27aff2f6e7ecca6
hyu <hyu2666@gmail.com>
2019-11-19 16:54:41 -0800

    Add payment resources

---
+++ src/main/java/edu/cmu/designit/server/managers/PaymentManager.java
@@ -0,0 +1,131 @@
+package edu.cmu.designit.server.managers;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import edu.cmu.designit.server.exceptions.AppException;
+import edu.cmu.designit.server.exceptions.AppInternalServerException;
+import edu.cmu.designit.server.models.Payment;
+import edu.cmu.designit.server.utils.MongoPool;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class PaymentManager extends Manager {
+
+    private static PaymentManager _self;
+    private MongoCollection<Document> paymentCollection;
+
+
+    private PaymentManager() {
+        this.paymentCollection = MongoPool.getInstance().getCollection("payments");
+    }
+
+    public static PaymentManager getInstance(){
+        if (_self == null)
+            _self = new PaymentManager();
+        return _self;
+    }
+
+    public void createPayment(Payment payment) throws AppException {
+        try {
+            Document newDoc = new Document()
+                    .append("userId", payment.getUserId())
+                    .append("amount", payment.getAmount())
+                    .append("status", payment.getStatus())
+                    .append("recruiterId", payment.getRecruiterId())
+                    .append("submissionId", payment.getSubmissionId())
+                    .append("createTime", payment.getCreateTime())
+                    .append("modifyTime", payment.getModifyTime());
+            if (newDoc != null) {
+                paymentCollection.insertOne(newDoc);
+            } else {
+                throw new AppInternalServerException(0, "Failed to create new payment relation");
+            }
+        } catch (Exception e) {
+            throw handleException("Create payment", e);
+        }
+    }
+
+    public ArrayList<Payment> getPaymentList() throws AppException {
+        try {
+            return convertDocsToArrayList(paymentCollection.find());
+        } catch(Exception e) {
+            throw handleException("Get Payment List", e);
+        }
+    }
+
+    public ArrayList<Payment> getPaymentById(String id) throws AppException {
+        try {
+            Bson filter = new Document("_id", new ObjectId(id));
+            return convertDocsToArrayList(paymentCollection.find(filter));
+        } catch (Exception e){
+            throw handleException("Get Single Payment", e);
+        }
+    }
+
+    public ArrayList<Payment> getPaymentByUser(String userId) throws AppException {
+        try {
+            Bson filter = new Document("userId", userId);
+            return convertDocsToArrayList(paymentCollection.find(filter));
+        } catch (Exception e){
+            throw handleException("Get Payment by user", e);
+        }
+    }
+
+    public ArrayList<Payment> getPaymentByRecruiter(String recruiterId) throws AppException {
+        try {
+            Bson filter = new Document("recruiterId", recruiterId);
+            return convertDocsToArrayList(paymentCollection.find(filter));
+        } catch (Exception e){
+            throw handleException("Get Payment by recruiter", e);
+        }
+    }
+
+    public void updatePayment(Payment payment) throws AppException {
+        try {
+            Bson filter = new Document("_id", new ObjectId(payment.getId()));
+            Bson newValue = new Document()
+                    .append("status", payment.getStatus())
+                    .append("modifyTime", payment.getModifyTime());
+            Bson updateOperationDocument = new Document("$set", newValue);
+
+            if (newValue != null)
+                paymentCollection.updateOne(filter, updateOperationDocument);
+            else
+                throw new AppInternalServerException(0, "Failed to update payment details");
+
+        } catch (Exception e) {
+            throw handleException("Update Comment", e);
+        }
+    }
+
+    public void deletePayment(String id) throws AppException {
+        try {
+            Bson filter = new Document("_id", new ObjectId(id));
+            paymentCollection.deleteOne(filter);
+        } catch (Exception e){
+            throw handleException("Delete payment", e);
+        }
+    }
+
+    private ArrayList<Payment> convertDocsToArrayList(FindIterable<Document> paymentDocs) {
+        ArrayList<Payment> paymentList = new ArrayList<>();
+        for(Document paymentDoc: paymentDocs) {
+            Payment payment = new Payment(
+                    paymentDoc.getObjectId("_id").toString(),
+                    paymentDoc.getString("userId"),
+                    paymentDoc.getDouble("amount"),
+                    paymentDoc.getInteger("status"),
+                    paymentDoc.getString("recruiterId"),
+                    paymentDoc.getString("submissionId"),
+                    (Date) paymentDoc.get("createTime"),
+                    (Date) paymentDoc.get("modifyTime")
+            );
+            paymentList.add(payment);
+        }
+        return paymentList;
+    }
+}
---
+++ src/main/java/edu/cmu/designit/server/models/Payment.java
@@ -0,0 +1,93 @@
+package edu.cmu.designit.server.models;
+
+import java.util.Date;
+
+public class Payment {
+    private String id;
+    private String userId;
+    private Double amount;
+    private int status; // 0 for not paid, 1 for paid
+    private String recruiterId;
+    private String submissionId;
+    private Date createTime;
+    private Date modifyTime;
+
+
+    public Payment(String id, String userId, Double amount, int status, String recruiterId,
+                   String submissionId, Date createTime, Date modifyTime) {
+        this.id = id;
+        this.userId = userId;
+        this.amount = amount;
+        this.status = status;
+        this.recruiterId = recruiterId;
+        this.submissionId = submissionId;
+        this.createTime = createTime;
+        this.modifyTime = modifyTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getRecruiterId() {
+        return recruiterId;
+    }
+
+    public void setRecruiterId(String recruiterId) {
+        this.recruiterId = recruiterId;
+    }
+
+    public String getSubmissionId() {
+        return submissionId;
+    }
+
+    public void setSubmissionId(String submissionId) {
+        this.submissionId = submissionId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
+
+
